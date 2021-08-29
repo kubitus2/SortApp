a51ded53fffa15e92a68dd7b87d1899afd19482b
kubitus2 <jakubosinski2@gmail.com>
2021-08-29 19:21:18 +0200

    Added delegate system.

--- SortApp/Assets/Scripts/CubesHandler.cs
+++ SortApp/Assets/Scripts/CubesHandler.cs
@@ -4,20 +4,24 @@ using UnityEngine;
 
 public class CubesHandler : MonoBehaviour
 {
-    [SerializeField]
-    private int numOfCubes = 10;
+    public delegate void SwapAction(GameObject a, GameObject b);
+    public static event SwapAction OnSwap;
 
     [SerializeField]
+    private int numOfCubes = 10;
+    [SerializeField]
     private GameObject prefab;
+
+    private bool isSortPlaying;
 
 
     private List<GameObject> cubes = new List<GameObject>();
 
 
-    // Start is called before the first frame update
-    void Awake()
+    void Start()
     {
         SpawnCubes(numOfCubes);
+        isSortPlaying = false;
     }
 
     void SpawnCubes(int n)
@@ -29,6 +33,7 @@ public class CubesHandler : MonoBehaviour
             pos = this.transform.position + new Vector3 (1.5f * (i - numOfCubes / 2), 0.0f, 0.0f);
             GameObject nextCube = Instantiate(prefab, pos, Quaternion.identity);
             nextCube.GetComponent<Renderer>().material.color = GetRandomGrayscaleColor();
+            nextCube.gameObject.layer = 3;
             cubes.Add(nextCube);
         }
     }
@@ -40,4 +45,56 @@ public class CubesHandler : MonoBehaviour
 
         return new Color(value, value, value, 1.0f);
     }
+
+    public void Sort()
+    {
+        //BubbleSort();
+
+
+    }
+
+    //SwapING ALGORITHMS
+
+    void Swap(int a, int b)
+    {
+        GameObject temp = cubes[b];
+        cubes[b] = cubes[a];
+        cubes[a] = temp;
+
+
+    }
+
+    bool IsBrighter(GameObject a, GameObject b)
+    {
+        return a.GetComponent<Renderer>().material.color.grayscale > b.GetComponent<Renderer>().material.color.grayscale;
+    }
+
+    
+    void BubbleSort()
+    {
+        for (int j = 0; j <= cubes.Count - 2; j++)
+        {
+            for (int i = 0; i <= cubes.Count - 2; i++)
+            {
+                if(IsBrighter(cubes[i], cubes[i+1]))
+                {
+                    Swap(i, i+1);
+                    if(OnSwap != null)
+                        OnSwap(cubes[i], cubes[i+1]);
+                }
+
+            }
+        }
+    }
+
+    public void Test()
+    {
+        OnSwap(cubes[2], cubes[3]);
+        OnSwap(cubes[5], cubes[4]);
+        Debug.Log("Test");
+
+    }
+
+
+
 }
---
+++ SortApp/Assets/Scripts/UFO.cs
@@ -0,0 +1,145 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class UFO : MonoBehaviour
+{
+    [SerializeField]
+    private float standardAltitude = 5.0f;
+    [SerializeField]
+    private float speed = 1f;
+
+
+
+    enum FloatMode
+    {
+        Directly,
+        Infront,
+        Behind
+    };
+    void OnEnable()
+    {
+        CubesHandler.OnSwap += EventTest;
+    }
+    void Start()
+    {
+        StartCoroutine(MoveAbove(new Vector3(3f,0f,0f), FloatMode.Directly));
+    }
+    
+    IEnumerator SwapAnimation(GameObject a, GameObject b)
+    {
+        Vector3 aPosition = a.transform.position;
+        Vector3 bPosition = b.transform.position;
+
+        yield return MoveAbove (aPosition, FloatMode.Directly);
+
+        yield return Lift();
+        yield return MoveAbove (bPosition, FloatMode.Infront);
+        yield return Drop();
+
+
+        yield return MoveAbove(bPosition, FloatMode.Directly);
+        yield return Lift();
+        yield return MoveAbove (aPosition, FloatMode.Directly);
+        yield return Drop();
+
+        yield return MoveAbove(bPosition, FloatMode.Infront
+        );
+        yield return Lift();
+        yield return MoveAbove(bPosition, FloatMode.Directly);
+        yield return Drop();
+    }
+
+    IEnumerator MoveAbove(Vector3 targetPlace, FloatMode mode)
+    {
+        Vector3 offset = Vector3.zero;
+
+        switch(mode)
+        {
+            case FloatMode.Directly:
+                offset = new Vector3 (0, standardAltitude, 0);
+                break;
+            case FloatMode.Infront:
+                offset = new Vector3 (0, standardAltitude, -2.0f);
+                break;
+            case FloatMode.Behind:
+                offset = new Vector3 (0, standardAltitude, 2.0f);
+                break;
+        }
+        Vector3 target = targetPlace + offset;
+        yield return MoveObject(this.transform, target);
+
+        yield return null;
+    }
+
+    IEnumerator MoveObject(Transform obj, Vector3 target)
+    {
+        obj.transform.DOMove(target, speed);
+        while(obj.transform.position != target)
+        {
+            yield return null;
+        }
+        yield return null;
+    }
+    
+    IEnumerator Lift()
+    {
+
+        Vector3 cargoPos = new Vector3 (transform.position.x, transform.position.y - 1.0f, transform.position.z);
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, Vector3.down);
+        GameObject cargo;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider != null)
+            {
+                cargo = hit.collider.gameObject;
+                yield return MoveObject(cargo.transform, cargoPos);
+                cargo.transform.parent = gameObject.transform;
+            }
+        }
+        
+        yield return null;
+    }
+
+    IEnumerator Drop()
+    {
+        Transform child = GetChildrenByLayer(3);
+
+        Vector3 dropPosition = new Vector3 (child.position.x, 0.5f, child.position.z);
+
+
+        child.parent = null;
+        
+        yield return MoveObject(child.transform, dropPosition);
+
+        yield return null;
+    }
+
+    Transform GetChildrenByLayer(int layerID)
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Transform child = transform.GetChild(i);
+
+            if(child.gameObject.layer == layerID)
+            {
+                return child;
+            }
+        }
+        return null;  
+    }
+
+    void EventTest(GameObject a, GameObject b)
+    {
+        StartCoroutine(SwapAnimation(a, b));
+    }
+
+    void OnDisable()
+    {
+        CubesHandler.OnSwap -= EventTest;
+    }
+
+}
