3ae47044b90ac64c62829b7ac649293c6c81ab9d
kubitus2 <jakubosinski2@gmail.com>
2021-08-30 18:20:18 +0200

    Added GUI click sound.

---
+++ SortApp/Assets/Scripts/GUIAudio.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GUIAudio : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip sound;
+
+    private Button button {get {return GetComponent<Button>();}}
+    private AudioSource source {get {return GetComponent<AudioSource>();}}
+
+
+    void Awake()
+    {
+        gameObject.AddComponent<AudioSource>();
+        source.clip = sound;
+        source.playOnAwake = false;
+
+        button.onClick.AddListener(() => PlaySound());
+    }
+
+
+    void PlaySound()
+    {
+        source.PlayOneShot(sound);
+    }
+}
